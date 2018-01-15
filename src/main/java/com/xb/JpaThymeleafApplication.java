fb0213130de9b3f7b17c318878400787276cb394
MrZhangshijie <549627593@qq.com>
2018-01-15 20:42:16 +0800

    张世杰提交北哥

---
+++ src/main/java/com/xb/JpaThymeleafApplication.java
@@ -0,0 +1,24 @@
+package com.xb;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+
+@SpringBootApplication
+@MapperScan("com.xb.mapper")
+public class JpaThymeleafApplication extends SpringBootServletInitializer {
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        System.out.println("33");
+        return application.sources(JpaThymeleafApplication.class);
+
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(JpaThymeleafApplication.class, args);
+    }
+}
+
