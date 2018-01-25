50b47e9ccf51c58427c8f8d21b813af5ee5bb2f5
MrZhangshijie <549627593@qq.com>
2018-01-25 14:17:14 +0800

    张世杰提交北哥asDsad sda s aSD

--- src/main/java/com/xb/JpaThymeleafApplication.java
+++ src/main/java/com/xb/JpaThymeleafApplication.java
@@ -12,7 +12,7 @@ import org.springframework.boot.web.support.SpringBootServletInitializer;
 public class JpaThymeleafApplication extends SpringBootServletInitializer {
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        System.out.println("3kkasd3");
+        System.out.println("3kkasoood3");
         return application.sources(JpaThymeleafApplication.class);
 
     }
